33dfcea7a86808a5109e85d22bb85dbf6827bf8c
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-19 18:47:37 +0900

    Created file to store Constants and API keys.

---
+++ AtmosWeather/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  AtmosWeather
+//
+//  Created by Brandon Jones on 6/19/24.
+//
+
+import Foundation
+
+
+struct Constants {
+  
+  struct Keys {
+    static let weatherAPIKey = "XXXX"
+  }
+}
