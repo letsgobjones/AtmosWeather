d671c41fb05d4021d99caed7d3f2ab83435b34dd
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-19 15:41:15 +0900

    Initial Commit

---
+++ AtmosWeather/AtmosWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmosWeatherApp.swift
+//  AtmosWeather
+//
+//  Created by Brandon Jones on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AtmosWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
